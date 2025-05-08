b51c9db8487b391e1a7a472dfffc009eaa538cd5
sahdu <halllllujah@gmail.com>
2025-05-08 19:11:07 +0800

    small fixes

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ let timeLeft = 25 * 60; // 25 minutes in seconds
 let isRunning = false;
 let startBtn = document.getElementById("startBtn");
 let defaultMode = 'focus'
+let currentMode = 'focus'
 let alarmSound = new Audio('imgs-audio/fist_full_of_dollars.mp3')
 
 function updateDisplay() {
     const minutes = Math.floor(timeLeft / 60);
     const seconds = timeLeft % 60;
-    document.getElementById("clock").innerHTML = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    const timeString = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+
+    document.getElementById("clock").innerHTML = timeString;
+    document.title = `${timeString} - ${currentMode === 'focus' ? 'Focus' : 'Break'}`;
 }
+
 function startTimer() {
     if (isRunning) return; // Prevent multiple timers from starting
     isRunning = true;
@@ -52,12 +57,12 @@ function switchMode(mode){
     document.getElementById('break').classList.remove("active-mode");
     document.getElementById(mode).classList.add("active-mode");
 
-    // update timeLeft depending on mode
+    currentMode = mode
     timeLeft = mode === 'focus' ? 25 * 60 : 5 * 60;
     updateDisplay();
 }
-window.onload = () => {
-    switchMode('focus');
+window.onload = () =>{
+    switchMode(defaultMode)
 }
 function featuresNotAdded() {
     alert("i havnet add these features lol")
